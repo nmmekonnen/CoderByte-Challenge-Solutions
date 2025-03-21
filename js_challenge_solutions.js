becd3481c37a5a665393a7957764f9e53ec57007
Nehemiah Mekonnen <nmmekonnen@gmail.com>
2025-03-21 17:57:46 -0400

    Add js_challenge_solutions.js file

---
+++ js_challenge_solutions.js
@@ -0,0 +1,84 @@
+//evaluate whether there is the same amount of x and os in a string
+
+function ExOh(str) { 
+
+    let arr = []; 
+  
+    let strMap = new Map(); 
+  
+    for(let i=0; i<str.length; i++){
+      if(strMap.has(str[i])){
+          strMap.set(str[i], strMap.get(str[i])+1)
+        } else {
+          strMap.set(str[i], 1)
+        }
+      }
+  
+    for(let [key, value] of strMap){
+      arr.push(value)
+    }
+  
+    for(let i=0; i<arr.length; i++){
+       if(arr[i]===arr[i + 1]){
+  
+         return true
+       } else {
+  
+         return false
+       }
+  
+        
+      }
+  
+  
+    }
+
+
+    //Identify the number in the array that appears the most w
+
+    function SimpleMode(arr) { 
+
+        let arrMap = new Map(); 
+      
+      
+        let max = 0; 
+      
+        let most = {}; 
+      
+        for(let i=0; i<arr.length; i++){
+          if(arrMap.has(arr[i])){
+      
+            arrMap.set(arr[i], arrMap.get(arr[i])+1)
+          } else {
+      
+            arrMap.set(arr[i], 1)
+          }
+         }
+      
+         for(let [key, value] of arrMap){
+           if(value > max){
+      
+             max = value
+      
+             most = key; 
+           }
+      
+         }
+      
+        if(most === 1){
+      
+          return -1
+        } else {
+      
+          return most
+      
+      
+        }
+           
+       
+      
+      
+      }
+         
+      // keep this function call here 
+      console.log(SimpleMode(readline()));
